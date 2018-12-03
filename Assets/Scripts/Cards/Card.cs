3e5dcedd8059ad7dab962beef6550f2d263644b7
Carlos Vallory <carlvallory@gmail.com>
2018-12-02 21:56:38 -0300

    Initial Commit TCG

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+
+        public new string name;
+        public string description;
+
+        public Sprite artwork;
+
+        public int manaCost;
+        public int attack;
+        public int health;
+        public int ability;
+
+        public string type;
+
+        //group
+        public string color;
+        public string ethnic;
+        public string myths;
+
+
+    }
+}
---
+++ Assets/Scripts/Cards/CardDisplay.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SA
+{
+    public class CardDisplay : MonoBehaviour
+    {
+
+        public Card card;
+
+        public Text nameText;
+        public Text description;
+
+        public Image artworkImage;
+
+        public Text manaText;
+        public Text attackText;
+        public Text healthText;
+        public Text abilityText;
+
+        public Text typeText;
+
+        // Use this for initialization
+        void Start()
+        {
+            LoadCard(card);
+        }
+
+        public void LoadCard(Card card)
+        {
+            if (card == null)
+            {
+                return;
+            }
+
+            nameText.text = card.name;
+            description.text = card.description;
+
+            artworkImage.sprite = card.artwork;
+
+            manaText.text = card.manaCost.ToString();
+            attackText.text = card.attack.ToString();
+            healthText.text = card.health.ToString();
+
+            if (string.IsNullOrEmpty(card.ability.ToString()))
+            {
+                abilityText.gameObject.SetActive(false);
+            } else {
+                abilityText.gameObject.SetActive(true);
+                abilityText.text = card.ability.ToString();
+            }
+
+            typeText.text = card.type;
+        }
+
+    }
+}
